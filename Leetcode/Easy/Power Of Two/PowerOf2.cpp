8cf9caf7249899aff64fdbbd240696f9a09735f1
Rahul Goswami <155483433+CodingWizard2001@users.noreply.github.com>
2024-03-12 22:18:48 +0530

    Update PowerOf2.cpp

--- Leetcode/Easy/Power Of Two/PowerOf2.cpp
+++ Leetcode/Easy/Power Of Two/PowerOf2.cpp
@@ -3,7 +3,8 @@
 #include<iostream>
 using namespace std;
 bool isPowerOfTwo(int n){
-    return (n & (n == 1)) == 0;
+    if(n <= 0) return false;
+    return (n & (n - 1)) == 0;
 }
 int main(){
     int n;
